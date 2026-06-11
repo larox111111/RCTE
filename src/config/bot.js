9d1531ec6b9f136bba487becf95cf9e7d97ba74a
larox111111 <loris.joffroy@gmail.com>
2026-06-11 09:54:54 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Match Premier de la RCTE",
+        name: "Match Premier de la RCTE | /help /rcte",
         // Activity type number (0 = Playing).
         type: 3, 
       },
