9c8a9f997f75785de8c6ba1417ed4dc91d8344a5
larox111111 <loris.joffroy@gmail.com>
2026-06-11 09:44:44 +0200

    Update twitchMonitor.js

--- src/services/twitchMonitor.js
+++ src/services/twitchMonitor.js
@@ -90,7 +90,7 @@ export async function startTwitchMonitor(client) {
                         .setTimestamp();
 
                     await notifChannel.send({
-                        content: `🔴 @everyone **${stream.user_name}** est maintenant en live sur Twitch !`,
+                        content: `🔴 **${stream.user_name}** est maintenant en live sur Twitch ! @everyone`,
                         embeds: [embed],
                     });
 
